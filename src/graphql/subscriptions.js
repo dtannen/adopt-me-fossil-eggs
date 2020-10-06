44ba22a57a550c3b4696d9a581b5e83bd0ffc8b4
dtannen <dtannen@gmail.com>
2020-10-05 21:43:18 -0400

    add amplify

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePet = /* GraphQL */ `
+  subscription OnCreatePet {
+    onCreatePet {
+      id
+      name
+      img_url
+      rarity
+      count
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePet = /* GraphQL */ `
+  subscription OnUpdatePet {
+    onUpdatePet {
+      id
+      name
+      img_url
+      rarity
+      count
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePet = /* GraphQL */ `
+  subscription OnDeletePet {
+    onDeletePet {
+      id
+      name
+      img_url
+      rarity
+      count
+      createdAt
+      updatedAt
+    }
+  }
+`;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports';
+import router from './router';
+
+Amplify.configure(aws_exports);
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
